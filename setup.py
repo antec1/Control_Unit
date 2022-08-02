b7e99eaaf575c88732c68badf362a2572171f5f2
antec1 <110452783+antec1@users.noreply.github.com>
2022-08-02 11:37:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name="HelloWorld",
-    version="0.1",
+    name="New_Script",
+    version="1.0.0",
     packages=find_packages(),
     scripts=['LTE.py']
 )
