69cf4d5e31cb285f66883473e4dd568ec353a165
Antenen Cédric Jan <cedricjan.antenen@students.bfh.ch>
2022-09-29 13:48:50 +0200

    Commit Pls

--- OTA/1.0.1/flash/main.py
+++
@@ -1,8 +0,0 @@
-import pycom
-import binascii
-
-from config import WIFI_SSID, WIFI_PW, SERVER_IP
-
-# Turn on GREEN LED
-pycom.heartbeat(False)
-pycom.rgbled(0xff0000)
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="New_Script",
-    version="1.0.0",
-    packages=find_packages(),
-    scripts=['main.py']
-)
